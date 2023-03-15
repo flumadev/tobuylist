65f00424510a9f3230365fd84e5c9323291b39f8
Luis Amorim <luisfelipeamorim@hotmail.com>
2023-03-15 01:16:37 -0300

    feat(preview): remoção dos links e input de waitlist para lançament

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,38 +3,39 @@ import Image from "next/image";
 import logo from '@/assets/logo.svg'
 import logoWhite from '@/assets/logo-white.svg'
 import Link from "next/link";
-export default function Footer(){
-    return(
+
+export default function Footer() {
+    return (
         <footer className={styles.footer}>
             <div className={styles.container}>
                 <div className={styles.company}>
-                    <Image src={logoWhite} alt={"To buy list"} width={38} height={38} />
+                    <Image src={logoWhite} alt={"To buy list"} width={38} height={38}/>
                     <h1>To buy list</h1>
                     <p>© Copyright 2023 Fluma.</p>
                 </div>
                 <div className={styles.links}>
                     <h3>ABOUT</h3>
-                    <Link href={"/"}>Team</Link>
-                    <Link href={"/"}>FAQ</Link>
-                    <Link href={"/"}>Careers</Link>
-                    <Link href={"/"}>Changelog</Link>
-                    <Link href={"/"}>Blog</Link>
+                    <Link aria-disabled href={"/"}>Team</Link>
+                    <Link aria-disabled href={"/"}>FAQ</Link>
+                    <Link aria-disabled href={"/"}>Careers</Link>
+                    <Link aria-disabled href={"/"}>Changelog</Link>
+                    <Link aria-disabled href={"/"}>Blog</Link>
                 </div>
                 <div className={styles.links}>
                     <h3>DOWNLOADS</h3>
-                    <Link href={"/"}>macOS</Link>
-                    <Link href={"/"}>Windows</Link>
-                    <Link href={"/"}>Linux</Link>
+                    <Link aria-disabled href={"/"}>macOS</Link>
+                    <Link aria-disabled href={"/"}>Windows</Link>
+                    <Link aria-disabled href={"/"}>Linux</Link>
                 </div>
                 <div className={styles.links}>
                     <h3>DEVELOPERS</h3>
-                    <Link href={"/"}>Documentation</Link>
-                    <Link href={"/"}>Contribute</Link>
+                    <Link aria-disabled href={"/"}>Documentation</Link>
+                    <Link aria-disabled href={"/"}>Contribute</Link>
                 </div>
                 <div className={styles.links}>
                     <h3>ORG</h3>
-                    <Link href={"/"}>Documentation</Link>
-                    <Link href={"/"}>Contribute</Link>
+                    <Link aria-disabled href={"/"}>Documentation</Link>
+                    <Link aria-disabled href={"/"}>Contribute</Link>
                 </div>
             </div>
         </footer>
