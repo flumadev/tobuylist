4b7c02da7b72a05c3f53d4cc7278b48cffb348c1
Luis Amorim <luisfelipeamorim@hotmail.com>
2023-03-25 01:30:50 -0300

    feat(modal): ajuste do modal de criação de lista e implementação do modal de itens

--- components/Modals/NewItem.tsx
+++ components/Modals/NewItem.tsx
@@ -3,14 +3,16 @@ import {Button} from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
 
-export default function NewList() {
+export default function NewItem() {
   const [open, setOpen] = useState<boolean>()
 
   function handleCancel() {
@@ -24,19 +26,21 @@ export default function NewList() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Nova lista</DialogTitle>
+          <DialogTitle>Adicionar lista</DialogTitle>
 
         </DialogHeader>
         <div className="flex flex-col gap-4 py-4">
-          <Input id="username" placeholder={"Nome"} autoComplete={"off"} className="col-span-3"/>
+          <Input id="username" placeholder={"Nome"} className="col-span-3"/>
           <div className="flex gap-4 w-full">
-
+            <Input id="name" placeholder={"Preço"} className="w-full"/>
+            <Input id="quantity" placeholder={"Quantidade"} className="w-1/4"/>
           </div>
-
+          <Input id="link" placeholder={"Link"} className="col-span-3"/>
+          <Input id="store" placeholder={"Nome da loja"} className="col-span-3"/>
         </div>
         <DialogFooter>
           <Button variant={"outline"} onClick={handleCancel}>Cancelar</Button>
-          <Button type="submit">Criar</Button>
+          <Button type="submit">Adicionar</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
