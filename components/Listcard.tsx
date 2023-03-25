134c7853e07bdc747bf5cff6c3f655b09577803e
Luis Amorim <luisfelipeamorim@hotmail.com>
2023-03-24 23:51:33 -0300

    feat(lists): criação dos cards de lista
    criação de card de lista e componente de tags

---
+++ components/Listcard.tsx
@@ -0,0 +1,40 @@
+import {Avatar, AvatarImage} from "@/components/ui/avatar";
+import {BookmarkIcon, LinkIcon, LockIcon} from "lucide-react";
+import Tag from "@/components/Tag";
+
+
+export default function Listcard() {
+  return (
+    <div className="flex flex-col gap-6 bg-white p-8 rounded-3xl max-w-[360px] max-h-[360px] dark:bg-slate-900">
+      <div className={"flex gap-2 "}>
+        <span>
+          <LockIcon />
+        </span>
+        <h1 className="text-2xl font-bold">Nome da lista</h1>
+        <span></span>
+      </div>
+      <h1 className={"text-3xl font-bold"}>R$ 25.000,00</h1>
+      <div className={"flex w-full flex-wrap gap-2"}>
+        {Array(5).fill(0).map((_, i) => {
+          return <Tag key={i} name={"computador"} />
+        })}
+      </div>
+      <div className={"flex items-center gap-2"}>
+        <div  className={"flex ml-3"}>
+          {Array(5).fill(0).map((_, i) => {
+            return (
+              <Avatar className={"ml-[-1rem]"}>
+                <AvatarImage src="https://avatars.githubusercontent.com/u/5020412?v=4"/>
+              </Avatar>
+            )
+          })}
+        </div>
+        <LinkIcon />
+      </div>
+      <div className={"flex justify-between"}>
+        <p>123 likes</p>
+        <BookmarkIcon />
+      </div>
+    </div>
+  )
+}
--- pages/api/lists/[pid].ts
+++ pages/api/lists/[pid].ts
@@ -1,9 +1,3 @@
-import { Prisma } from "@prisma/client"
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "pages/api/auth/[...nextauth]"
-
-import { prisma } from "@/lib/client.prisma"
-import {createList} from "@/backend/lists/create";
 import {getList} from "@/backend/lists/get";
 
 export default async function handler(req, res) {
